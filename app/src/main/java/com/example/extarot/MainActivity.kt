7f608551a6c0c49a8e37217128b7f4036753ede0
pinwormmy <ozkhan@naver.com>
2023-04-13 16:40:29 +0900

    Initial commit 타로 어플
    메인화면만 초간단 버전으로 구현한 상태

---
+++ app/src/main/java/com/example/extarot/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.extarot
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val isDarkTheme = isSystemInDarkTheme()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "ExTarot",
+            fontSize = 50.sp,
+            color = if (isDarkTheme) Color.White else Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = {
+            // 카드 뽑기 화면으로 이동하는 코드를 여기에 작성하세요.
+        }) {
+            Text(
+                text = "타로 카드 뽑기",
+                color = if (isDarkTheme) Color.White else Color.Black
+            )
+        }
+    }
+}
+
