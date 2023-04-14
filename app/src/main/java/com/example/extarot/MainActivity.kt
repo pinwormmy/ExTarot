b81b7e7bc856b79cd9f60289f82163e9ddba9eef
pinwormmy <ozkhan@naver.com>
2023-04-14 17:08:01 +0900

    jetpack 적용부터 다시 해야할 듯
    호환성 꼬이면서 엉망됨 ㅋ

--- app/src/main/java/com/example/extarot/MainActivity.kt
+++ app/src/main/java/com/example/extarot/MainActivity.kt
@@ -2,17 +2,8 @@ package com.example.extarot
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
+import com.example.extarot.TarotCardScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,26 +17,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val isDarkTheme = isSystemInDarkTheme()
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "ExTarot",
-            fontSize = 50.sp,
-            color = if (isDarkTheme) Color.White else Color.Black
-        )
+    val navController = rememberNavController()
 
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Button(onClick = {
-            // 카드 뽑기 화면으로 이동하는 코드를 여기에 작성하세요.
-        }) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
-                text = "타로 카드 뽑기",
+                text = "ExTarot",
+                fontSize = 50.sp,
                 color = if (isDarkTheme) Color.White else Color.Black
             )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(onClick = { navController.navigate("tarot_card_screen") }) {
+                Text(
+                    text = "타로 카드 뽑기",
+                    color = if (isDarkTheme) Color.White else Color.Black
+                )
+            }
+        }
+
+        NavHost(navController = navController, startDestination = "main_screen") {
+            composable("main_screen") { /* 메인 스크린을 나타내는 Composable 함수를 작성하세요. */ }
+            composable("tarot_card_screen") { TarotCardScreen() }
         }
     }
 }
---
+++ app/src/main/java/com/example/extarot/TarotCardScreen.kt
@@ -0,0 +1,9 @@
+// TarotCardScreen.kt
+package com.example.extarot
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TarotCardScreen() {
+    // 카드 뽑기 화면을 작성하는 코드를 여기에 작성하세요.
+}
