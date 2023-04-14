35a09187bca00fc86a403e5b7f94d7c7fae9fab4
pinwormmy <ozkhan@naver.com>
2023-04-15 00:14:59 +0900

    집에서 커밋테스트(버전 수정해서 오류 다 잡음)
    gpt기준으로 코딩하다보니 버전 다운그레이드 됨(감안하고 가자)

--- app/src/main/java/com/example/extarot/MainActivity.kt
+++ app/src/main/java/com/example/extarot/MainActivity.kt
@@ -2,8 +2,27 @@ package com.example.extarot
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.extarot.TarotCardScreen
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
