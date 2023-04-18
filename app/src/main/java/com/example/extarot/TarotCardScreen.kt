ee107c88a59d608130a8fa560cde85cad6507d49
pinwormmy <ozkhan@naver.com>
2023-04-18 16:56:57 +0900

    카드 뭉치(덱) 그래픽 작업 진행 중...
    원래는 카드뒷면을 위로 본 시점에서
    여러카드가 약간 삐뚤빼뚤 뭉쳐진 형태로 구현되어야 하나,
    지금은 뭉쳐진 카드가 육면체 형태로 쌓여 있고,
    모서리 둥근 처리가 제대로 안 되는 중.
    일단 모서리 처리부터 제대로 완료하고, 육면체 문제 해결하기

--- app/src/main/java/com/example/extarot/TarotCardScreen.kt
+++ app/src/main/java/com/example/extarot/TarotCardScreen.kt
@@ -1,14 +1,18 @@
 package com.example.extarot
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
@@ -22,9 +26,12 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,7 +49,7 @@ fun TarotCardScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                TarotDeck(isDarkTheme)
+                TarotDeck(isDarkTheme, 78, 200.dp, 2.dp) // 간격 조정
 
                 Spacer(modifier = Modifier.height(16.dp))
 
@@ -53,12 +60,31 @@ fun TarotCardScreen() {
 }
 
 @Composable
-fun TarotDeck(isDarkTheme: Boolean) {
-    Text(
-        text = stringResource(R.string.tarot_deck),
-        color = if (isDarkTheme) Color.White else MaterialTheme.colors.onBackground
-    )
+fun TarotDeck(isDarkTheme: Boolean, maxCards: Int, cardSize: androidx.compose.ui.unit.Dp, cardGap: androidx.compose.ui.unit.Dp) {
+    Box(
+        modifier = Modifier.size(cardSize),
+        contentAlignment = Alignment.Center
+    ) {
+        val cardBackImage = painterResource(R.drawable.card_back)
+
+        repeat(maxCards) { index ->
+            Image(
+                painter = cardBackImage,
+                contentDescription = stringResource(R.string.tarot_deck),
+                modifier = Modifier
+                    .graphicsLayer(
+                        translationX = (index * cardGap.value),
+                        translationY = (index * cardGap.value)
+                    )
+                    .size(cardSize)
+                    .clip(RoundedCornerShape(70.dp)) // 각 모서리 둥글게 처리
+            )
+        }
+    }
 }
+
+
+
 
 @Composable
 fun ShuffleButton(isDarkTheme: Boolean) {
@@ -78,6 +104,7 @@ fun ShuffleButton(isDarkTheme: Boolean) {
             contentDescription = null,
             tint = contentColor,
             modifier = Modifier.padding(end = 8.dp)
+
         )
 
         Text(
@@ -101,4 +128,14 @@ fun DarkPreview() {
     MaterialTheme(darkColors()) {
         TarotCardScreen()
     }
+}
+
+private fun getRandomAngle(): Float {
+    return (-5..5).random().toFloat()
+}
+
+private fun getRandomTranslation(): Pair<Float, Float> {
+    val x = (-10..10).random().toFloat()
+    val y = (-10..10).random().toFloat()
+    return Pair(x, y)
 }
