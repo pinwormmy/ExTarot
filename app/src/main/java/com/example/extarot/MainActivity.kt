6571d1d44ffbbb56f81579c10b65ffedba764e6d
pinwormmy <ozkhan@naver.com>
2023-04-18 15:30:03 +0900

    메인 화면 구성하고, 카드 섞는 화면 별도로 만듬
    화면 전환까지 구현 완료
    근데 카드를 안 보여주고 그냥 '카드뭉치'라는 텍스트만 써있음 ㅋㅋ
    바로 밑에 카드 섞기 버튼은 구현 완료
    
    -그래픽 작업도 진행하기

--- app/src/main/java/com/example/extarot/MainActivity.kt
+++ app/src/main/java/com/example/extarot/MainActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.extarot.TarotCardScreen
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 
@@ -38,34 +37,35 @@ fun MainScreen() {
     val isDarkTheme = isSystemInDarkTheme()
     val navController = rememberNavController()
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "ExTarot",
-                fontSize = 50.sp,
-                color = if (isDarkTheme) Color.White else Color.Black
-            )
+    NavHost(navController = navController, startDestination = "main_screen") {
+        composable("main_screen") {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "ExTarot",
+                        fontSize = 50.sp,
+                        color = if (isDarkTheme) Color.White else Color.Black
+                    )
 
-            Spacer(modifier = Modifier.height(32.dp))
+                    Spacer(modifier = Modifier.height(32.dp))
 
-            Button(onClick = { navController.navigate("tarot_card_screen") }) {
-                Text(
-                    text = "타로 카드 뽑기",
-                    color = if (isDarkTheme) Color.White else Color.Black
-                )
+                    Button(onClick = { navController.navigate("tarot_card_screen") }) {
+                        Text(
+                            text = "타로 카드 뽑기",
+                            color = if (isDarkTheme) Color.White else Color.Black
+                        )
+                    }
+                }
             }
         }
-
-        NavHost(navController = navController, startDestination = "main_screen") {
-            composable("main_screen") { /* 메인 스크린을 나타내는 Composable 함수를 작성하세요. */ }
-            composable("tarot_card_screen") { TarotCardScreen() }
-        }
+        composable("tarot_card_screen") { TarotCardScreen() }
     }
 }
+
 
--- app/src/main/java/com/example/extarot/TarotCardScreen.kt
+++ app/src/main/java/com/example/extarot/TarotCardScreen.kt
@@ -1,9 +1,104 @@
-// TarotCardScreen.kt
 package com.example.extarot
 
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.darkColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Shuffle
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TarotCardScreen() {
-    // 카드 뽑기 화면을 작성하는 코드를 여기에 작성하세요.
+    val isDarkTheme = isSystemInDarkTheme()
+
+    MaterialTheme(
+        colors = if (isDarkTheme) darkColors() else lightColors()
+    ) {
+        Surface(color = MaterialTheme.colors.background) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                TarotDeck(isDarkTheme)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ShuffleButton(isDarkTheme)
+            }
+        }
+    }
+}
+
+@Composable
+fun TarotDeck(isDarkTheme: Boolean) {
+    Text(
+        text = stringResource(R.string.tarot_deck),
+        color = if (isDarkTheme) Color.White else MaterialTheme.colors.onBackground
+    )
+}
+
+@Composable
+fun ShuffleButton(isDarkTheme: Boolean) {
+    val icon: ImageVector = Icons.Outlined.Shuffle
+    val shuffleString: String = stringResource(R.string.shuffle)
+
+    val backgroundColor = if (isDarkTheme) colorResource(id = R.color.purple_500) else Color.White
+    val contentColor = if (isDarkTheme) Color.White else MaterialTheme.colors.onBackground
+
+    Button(
+        onClick = { /* TODO: 카드 섞기 애니메이션 코드 작성 */ },
+        colors = ButtonDefaults.buttonColors(backgroundColor = backgroundColor),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = contentColor,
+            modifier = Modifier.padding(end = 8.dp)
+        )
+
+        Text(
+            text = shuffleString,
+            color = contentColor
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LightPreview() {
+    MaterialTheme {
+        TarotCardScreen()
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DarkPreview() {
+    MaterialTheme(darkColors()) {
+        TarotCardScreen()
+    }
 }
