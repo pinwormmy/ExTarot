1694fb336e5a79fdcdaeab43e342da5342467b00
pinwormmy <ozkhan@naver.com>
2023-05-08 10:30:04 +0900

    카드 선택화면에서 선택 구현 중...
    드래그 액션 적용하려고 하는데, 반응이 없음
    (터치 선택까지 안되게 함)
    어느 지점 문제인지 확인 필요
    
    -카드 정확하게 집기 어렵기 때문에 드래그해서 선택하게 조정하기
    - 시간날 때마다 코드 분석 및 모르는 구문 공부하기

--- app/src/main/java/com/example/extarot/DrawCardScreen.kt
+++ app/src/main/java/com/example/extarot/DrawCardScreen.kt
@@ -3,18 +3,22 @@ package com.example.extarot
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
@@ -35,7 +39,7 @@ fun DrawCardsScreen(navController: NavController) {
     ) {
         cards.forEachIndexed { index, card ->
             val offsetX = if (index < totalCardsPerColumn) 0.dp else cardSize + cardSpacing
-            val offsetY = remember { mutableStateOf(-300.dp) }
+            val offsetY = remember { mutableStateOf((-300).dp) }
 
             LaunchedEffect(key1 = index) {
                 delay(50L * index)
@@ -51,17 +55,53 @@ fun DrawCardsScreen(navController: NavController) {
             val rotationDegrees = if (isSelected) 30f else 0f
             val animatedRotation by animateFloatAsState(targetValue = rotationDegrees)
 
-            TarotCard(
+            Box(
                 modifier = Modifier
                     .size(cardSize)
                     .offset(x = offsetX, y = animatedOffsetY)
-                    .rotate(animatedRotation) // 기본 상태에서 가로로 눕히고, 선택 시 30도 회전
                     .clip(RoundedCornerShape(cornerRadius)) // 이미지 모서리 곡선 적용
-                    .clickable { selectedCard.value = if (isSelected) null else card },
-                card = card,
-                faceUp = false,
-                rotate = true // 기본 상태에서 가로로 눕히기 위해 rotate 속성을 true로 설정
-            )
+            ) {
+                TarotCard(
+                    modifier = Modifier
+                        .size(cardSize)
+                        .rotate(animatedRotation) // 기본 상태에서 가로로 눕히고, 선택 시 30도 회전
+                    ,
+                    card = card,
+                    faceUp = false,
+                    rotate = true
+                )
+
+                val dragGestureModifier = Modifier.pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragStart = { offset ->
+                            val isDraggedOver = offset.y >= animatedOffsetY.value &&
+                                    offset.y <= animatedOffsetY.value + cardSize.value
+
+                            if (isDraggedOver) {
+                                selectedCard.value = card
+                            }
+                        },
+                        onDrag = { _, dragAmount ->
+                            val isDraggedOver = dragAmount.y >= animatedOffsetY.value &&
+                                    dragAmount.y <= animatedOffsetY.value + cardSize.value
+
+                            if (isDraggedOver) {
+                                selectedCard.value = card
+                            } else {
+                                selectedCard.value = null
+                            }
+                        },
+                        onDragEnd = {
+                            if (selectedCard.value == card) {
+                                selectedCard.value = null
+                            }
+                        }
+
+                    )
+                }.matchParentSize()
+
+                Box(modifier = dragGestureModifier) {}
+            }
         }
     }
 }
