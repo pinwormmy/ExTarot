a19d3ae405d67e7af37b03cc2b03a4114a339350
pinwormmy <ozkhan@naver.com>
2023-05-10 15:36:20 +0900

    선택 카드 누르면 뒤집어지고, 한번더 누르면 카드 설명 뜸
    카드 뒤집기는 한번만 하게 함.
    카드 설명창은 누르면 닫혀서 카드 앞면 다시 볼 수 있음
    와중에 카드 이름, 설명란을 추가한 TarotCard 데이타 클래스와
    List함수를 구성해야 하는데, 이 두개 함수는
    TarotCardScreen에 있던 걸 새 파일로 따로 뺄 거임.
    지금은 이름, 설명 변수 추가된 것때문에 오류난 상태.
    카드 설명창 뜨는 것까지 확인해봐야함.(테스트 아직 안 해봄)
    
    -카드 정확하게 집기 어렵기 때문에 드래그해서 선택하게 조정하기
    - 카드 섞기 애니메이션에서 따로 빼낸 카드는 앞으로 섞게 수정해야함
    - 시간날 때마다 코드 분석 및 모르는 구문 공부하기

--- app/src/main/java/com/example/extarot/CardDetailScreen.kt
+++ app/src/main/java/com/example/extarot/CardDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.example.extarot
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,25 +11,26 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CardDetailScreen(card: TarotCard) {
     var isRevealed by remember { mutableStateOf(false) }
+    var isOverlayVisible by remember { mutableStateOf(false) }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        contentAlignment = Alignment.Center
     ) {
         Box(
             modifier = Modifier
                 .size(500.dp)
-                .clip(RoundedCornerShape(84.dp)) // CircleShape 대신 RoundedCornerShape를 사용하였습니다.
-                .clickable { isRevealed = !isRevealed }
+                .clip(RoundedCornerShape(84.dp))
+                .clickable { isRevealed = !isRevealed },
         ) {
             if (!isRevealed) {
                 Image(
@@ -42,6 +44,36 @@ fun CardDetailScreen(card: TarotCard) {
                     style = MaterialTheme.typography.h4,
                     modifier = Modifier.align(Alignment.Center)
                 )
+            }
+        }
+
+        if (isRevealed) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.6f))
+                    .clickable { isOverlayVisible = !isOverlayVisible },
+                contentAlignment = Alignment.Center
+            ) {
+                if (isOverlayVisible) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = card.name,
+                            style = MaterialTheme.typography.h5,
+                            color = Color.White
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = card.description,
+                            style = MaterialTheme.typography.body1,
+                            color = Color.White
+                        )
+                    }
+                }
             }
         }
     }
